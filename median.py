135368e5d279e8131fd171018365c5feb1f029ca
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-03 21:00:06 +0100

    Add median calculator implementation.

--- median.py
+++ median.py
@@ -5,6 +5,18 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+
+        numbers.sort()
+        median_index = 0
+
+        if len(numbers) % 2 == 1:
+            median_index = len(numbers) // 2
+            print(numbers[median_index])
+        else:
+            median_index = (len(numbers) - 1) // 2
+            arith_mean = (numbers[median_index] + numbers[median_index + 1]) / 2
+            print(arith_mean)
+            
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
