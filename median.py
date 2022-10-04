3b4444f7810b572bb0faaeb0f94e3d0a80af9e4a
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-04 11:41:47 +0100

    Change position of return statement.

--- median.py
+++ median.py
@@ -8,15 +8,17 @@ while True:
 
         numbers.sort()
         median_index = 0
+        median_value = 0;
 
         if len(numbers) % 2 == 1:
             median_index = len(numbers) // 2
-            return int(numbers[median_index])
+            median_value = int(numbers[median_index])
         else:
             median_index = (len(numbers) - 1) // 2
             arith_mean = (numbers[median_index] + numbers[median_index + 1]) / 2
-            return float(arith_mean)
+            median_value = float(arith_mean)
 
+        return median_value
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
